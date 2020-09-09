664aa6700739fa0ef46590ac09f800918d003e69
ms4hafiz <ms4hafiz@live.com>
2020-09-09 18:49:29 +0430

    Reload with few changes

---
+++ Models/Analysis/totalpivotcombinedn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataSystem.Models.Analysis
+{
+    [Table("totalpivotcombinedn_final")]
+    public class totalpivotcombinedp
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string ProvinceId { get; set; }
+        public string Province { get; set; }
+        public string DistrictId { get; set; }
+        public string District { get; set; }
+        public int FacilityID { get; set; }
+        public string FacilityName { get; set; }
+        public int FacilityTypeId { get; set; }
+        public string TypeAbbrv { get; set; }
+        public int IndicatorId { get; set; }
+        public string IndicatorName { get; set; }
+        public string Implementer { get; set; }
+        public string Module { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int mYear { get; set; }
+        public int mMonth { get; set; }
+        public int Quarter { get; set; }
+        public int mQuarter { get; set; }
+        public int Num { get; set; }
+        public int Denom { get; set; }
+    }
+}
---
+++ Models/Analysis/totalpivotcombinedndist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataSystem.Models.Analysis
+{
+    [Table("totalpivotcombinedn_dist_final")]
+    public class totalpivotcombinedndist
+    {
+        [Key]
+        public string Id { get; set; } 
+        public string ProvinceId { get; set; }
+        public string Province { get; set; }
+        public string DistrictId { get; set; }
+        public string District { get; set; }
+        public int FacilityTypeId { get; set; }
+        public string TypeAbbrv { get; set; }
+        public int IndicatorId { get; set; }
+        public string IndicatorName { get; set; }
+        public string Implementer { get; set; }
+        public string Module { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int mYear { get; set; }
+        public int mMonth { get; set; }
+        public int Quarter { get; set; }
+        public int mQuarter { get; set; }
+        public int Num { get; set; }
+    }
+}
